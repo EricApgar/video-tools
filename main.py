03a2d125fd5db40dfd77b657bf9e80b6cd684511
EricApgar <ericapgar1@gmail.com>
2025-06-13 22:52:56 -0500

    Updates.

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import os
 import subprocess
-from moviepy.video.io.VideoFileClip import VideoFileClip
+# from moviepy.video.io.VideoFileClip import VideoFileClip
+from moviepy import *
 import pygame
 
 # 1. Download a YouTube video (clip of specific duration) efficiently
